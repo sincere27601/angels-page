2be630897cf33a555d1cd4ed88a7e11ded36371d
michaelraysor <michaelraysor1@gmail.com>
2021-10-21 18:06:20 -0400

    Updated intro color

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-purple-600" href="https://nextjs.org">
             A.E. Notary Signings 
           </a>
         </h1>
@@ -25,7 +25,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-           A.E Signings 
+           A.E Notary Signings 
         </a>
       </footer>
     </div>
